08f4003a0245f9e8ca4e4fa050fc84e14a152751
Muhammad Ibrahim <ibrahimjhullan7@gmail.com>
2023-02-12 11:57:49 +0500

    Type Casting

---
+++ Java_Basic/src/TypeCastingTest.java
@@ -0,0 +1,11 @@
+public class TypeCastingTest {
+    public static void main(String[] args) {
+        // implicit byte -> short -> int->
+        // float -> double
+
+        // Explicit double -> float    , int-> short -> byte
+
+    int intV=10;
+    byte byteV= (byte) intV;
+    }
+}
