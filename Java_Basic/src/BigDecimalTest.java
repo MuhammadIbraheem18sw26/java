c1970c9b22c667cfbd8ad2b1eeb1f6550b908831
Muhammad Ibrahim <ibrahimjhullan7@gmail.com>
2023-02-12 11:08:54 +0500

    Big Decimal class for rounding operation

---
+++ Java_Basic/src/BigDecimalTest.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+    public static void main(String[] args) {
+
+        BigDecimal value1= new BigDecimal("25.33");
+        BigDecimal value2= new BigDecimal("33.03");
+        System.out.println(value1.add(value2));
+    }
+
+}
--- Java_Basic/src/Main.java
+++ Java_Basic/src/Main.java
@@ -1,5 +1,17 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.printf("Hi ");
+        char b='$';
+        double a=10.1;
+        double c=10.1;
+        BigDecimal value1= new BigDecimal("10.01");
+        BigDecimal value2= new BigDecimal("10.01");
+                System.out.println(value1.add(value2));
+        System.out.println(a+c);
+        System.out.println("\u00BB");
+
     }
 }
