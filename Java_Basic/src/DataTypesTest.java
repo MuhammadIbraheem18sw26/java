3b9b73674c2ce317fa50b10e218d30ece74dea1a
Muhammad Ibrahim <ibrahimjhullan7@gmail.com>
2023-02-12 11:18:12 +0500

    Data types sample

---
+++ Java_Basic/src/DataTypesTest.java
@@ -0,0 +1,20 @@
+public class DataTypesTest {
+
+    public static void main(String[] args) {
+        int intergerValue=10;
+        long longValue=233333333;
+        byte byteValue= 123;
+
+        double doubleValue=10.11222222222d;
+        float floatValue=10.333f;
+
+        boolean booleanValue= true;
+
+        String stringValue= "Test String ";
+        char charValue='\u00BB';
+
+        System.out.println(intergerValue + " "+ longValue+ " "+ byteValue+ " "+ doubleValue+" "+ floatValue+" " +booleanValue+" "+" "+stringValue+" "+ charValue);
+
+
+    }
+}
